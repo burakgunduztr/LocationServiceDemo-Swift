71de3d2b543fc7410905292e1705b02454a94e7e
burakgunduztr <burakgunduz@outlook.com.tr>
2019-11-25 19:51:41 +0300

    Initial Commit

---
+++ location-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  location-test
+//
+//  Created by Burak Gunduz on 25.11.2019.
+//  Copyright © 2019 Burak Gunduz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
